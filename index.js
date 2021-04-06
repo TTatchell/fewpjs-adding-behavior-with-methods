08bc3409fe18171ff891074c569729b0b4363fc6
TTatchell <tim.tatchell24@gmail.com>
2021-04-06 21:08:00 +1000

    upload

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+class Cat {
+    constructor(name) {
+        this.name = name;
+
+    }
+
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    speak() {
+        switch (this.gender) {
+            case 'male':
+                return `It's me! ${this.name}, the parrot!`
+                
+            case 'female':
+                return `${this.name} says squawk!`
+            default:
+                break;
+        }
+        
+    }
+}
